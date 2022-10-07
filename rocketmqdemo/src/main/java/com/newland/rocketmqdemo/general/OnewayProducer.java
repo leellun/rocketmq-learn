ce59c1335e352bf21ffc94ea058dfbe756542f74
leellun <leellun@sina.cn>
2022-10-07 23:21:44 +0800

    rocketmq 5.0.0

---
+++ rocketmqdemo/src/main/java/com/newland/rocketmqdemo/general/OnewayProducer.java
@@ -0,0 +1,21 @@
+package com.newland.rocketmqdemo.general;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+public class OnewayProducer {
+    public static void main(String[] args) throws Exception{
+        DefaultMQProducer producer = new DefaultMQProducer("pg");
+        producer.setNamesrvAddr("192.168.66.70:8876;192.168.66.70:8878;192.168.66.70:8879");
+        producer.start();
+
+        for (int i = 0; i < 10; i++) {
+            byte[] body = ("Hi," + i).getBytes();
+            Message msg = new Message("single", "someTag", body);
+            // 单向发送
+            producer.sendOneway(msg);
+        }
+        producer.shutdown();
+        System.out.println("producer shutdown");
+    }
+}
